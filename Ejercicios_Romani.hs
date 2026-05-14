c6c161a69e8f91d156f804d38ebd926040aecbc8
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2026-05-13 19:35:49 -0500

    Ejercicios

--- Ejercicios_Romani.hs
+++ Ejercicios_Romani.hs
@@ -14,5 +14,18 @@ main = do
     putStrLn $ "La suma de la lista '" ++  show n ++ "' es: " ++ show r
 -}
 
-lista :: [Int]
-lista ma = 
+lista :: [Int] -> [Int]
+lista e = filter (>=18) e
+lista2 :: [Int] -> [Int]
+lista2 em = filter (<18) em
+
+main :: IO()
+main = do
+    putStrLn "Ingrese:"
+    input <- getLine
+    let n = read input :: [Int]
+    let r = lista n
+    let rm = lista2 n
+    putStrLn $ "Lista de edades: " ++ show n
+    putStrLn $ "Lista de menores: " ++ show rm
+    putStrLn $ "Lista de mayores: " ++ show r
